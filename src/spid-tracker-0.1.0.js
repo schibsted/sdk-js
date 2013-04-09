cdaa46e9fd0077c45bd5f7944c60db54d353eb4e
joawan <joakim.wanggren@gmail.com>
2013-04-09 17:27:48 +0200

    Rewrote tracker without jquery

--- src/spid-tracker-0.1.0.js
+++ src/spid-tracker-0.1.0.js
@@ -1,4 +1,110 @@
-/*global $ */
+/*global VGS */
+(function(win, v) {
+
+    var config = {
+        pulseServer      : 'http://127.0.0.1:8080/pulse/rest/analytics/report',
+        cookiePrefix     : 'spd_pls_',
+        throttlingFactor : 1.0
+    };
+
+    function on(selector, event, callback) {
+        function getHandler() {
+            if(win.addEventListener) {
+                return function(element, event, callback) { element.addEventListener(event, callback, false); };
+            }
+            if(win.attachEvent) {
+                return function(element, event, callback) { element.attachEvent('on'+event, function() { callback.call(this, win.event); }); };
+            }
+            return function(element, event, callback) {
+                var old = element["on"+event] ? element["on"+event] : function() {};
+                element["on"+event] = function(e) {
+                    if (!e) {
+                        e = win.event;
+                    }
+                    old.call(this, e);
+                    callback.call(this, e);
+                };
+            };
+        }
+        var handler = getHandler();
+        var elements;
+        if(typeof selector === 'string') {
+            elements = Array.prototype.slice.call(win.document.getElementsByTagName(selector));
+        } else {
+            elements = [selector];
+        }
+        for(var i in elements) {
+            handler(elements[i], event, callback);    
+        }
+    }
+
+    function updateCookie(name, days, minutes) {
+        function setCookie(value) {
+            var exdate = new Date();
+            exdate.setDate(exdate.getDate() + days);
+            exdate.setMinutes(exdate.getMinutes() + minutes);
+            var c_value = value + "; expires=" + exdate.toUTCString();
+            document.cookie = name + "=" + c_value;
+        }
+
+        function getCookie() {
+            var i, x, y, biscuits = document.cookie.split(";");
+            for (i = 0; i < biscuits.length; i++) {
+                x = biscuits[i].substr(0, biscuits[i].indexOf("="));
+                y = biscuits[i].substr(biscuits[i].indexOf("=") + 1);
+                x = x.replace(/^\s+|\s+$/g, "");
+                if (x === name) {
+                    return y;
+                }
+            }
+            return null;
+        }
+
+        var value = getCookie();
+        if (value === null) {
+            value = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+        setCookie(value);
+        return value;
+    }
+
+    function report() {
+
+        var uid = updateCookie(config.cookiePrefix + 'uid', 365, 0),
+            sid = updateCookie(config.cookiePrefix + 'sid', 0, 15),
+            pageLoadTime = new Date().getTime();
+
+        function pulse(attr) {
+            var payload = { url: win.document.URL, agent: win.navigator.userAgent, uid: uid, sid: sid, arrive: pageLoadTime, throttle: config.throttlingFactor, spid: spid };
+            for (var a in attr) {
+                payload[a] = attr[a];
+            }
+            win.console.log(JSON.stringify(payload));
+        }
+
+        //This will trigger on every link click
+        on('a', 'click', function() {
+            pulse({toUrl: this.getAttribute('href'), leave: new Date()});
+        });
+        //This will trigger on every unload, meaning also when linked is clicked
+        on(win, 'beforeunload', function() {
+            pulse({leave: new Date()});
+        });
+    }
+
+    var spid,
+        executed = false;
+    v.Event.subscribe('auth.sessionChange', function(data) {
+        spid = data.session ? data.session.userId : 0;
+        if (Math.random() <= config.throttlingFactor && !executed) { report(); executed = true; }
+    });
+}(window, VGS));
+
+
+/*
 $(document).ready(function () {
     try {
         function reportStatistics() {
@@ -33,7 +139,7 @@ $(document).ready(function () {
 
                 var value = getCookie();
 
-                if (value == null) {
+                if (value === null) {
                     value = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
                         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
                         return v.toString(16);
@@ -134,3 +240,4 @@ $(document).ready(function () {
     } catch (e) {
     }
 });
+*/
