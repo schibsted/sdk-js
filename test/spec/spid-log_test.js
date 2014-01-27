89a42404d5a0e70cb89e51511cd6e71d0993f4c2
joawan <joakim.wanggren@gmail.com>
2014-01-28 00:02:32 +0100

    Added tests for log error and info

--- test/spec/spid-log_test.js
+++ test/spec/spid-log_test.js
@@ -12,6 +12,22 @@ describe('SPiD.Log()', function() {
         SPiD.init(conf);
         assert.isTrue(SPiD.Log().enabled());
     });
+    it('SPiD.Log().error should log to console.error', function(done) {
+        var copy_error = window.console.error;
+        window.console.error = function() { done(); };
+        var conf = {client_id : '4d00e8d6bf92fc8648000000', server: 'stage.payment.schibsted.se', prod:false, logging:true};
+        SPiD.init(conf);
+        SPiD.Log().error('My message');
+        window.console.error = copy_error;
+    });
+    it('SPiD.Log().info should log to console.info', function(done) {
+        var copy_info = window.console.info;
+        window.console.info = function() { done(); };
+        var conf = {client_id : '4d00e8d6bf92fc8648000000', server: 'stage.payment.schibsted.se', prod:false, logging:true};
+        SPiD.init(conf);
+        SPiD.Log().info('My message');
+        window.console.info = copy_info;
+    });
     it('SPiD.Log().enabled should return false', function() {
         var conf = {client_id : '4d00e8d6bf92fc8648000000', server: 'stage.payment.schibsted.se', prod:false, logging:false};
         SPiD.init(conf);
