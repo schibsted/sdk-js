a3453641f755e597c111be8ed5cb6f8067887c08
juszkiewicz <marcin.juszkiewicz@schibsted.pl>
2016-07-25 12:21:54 +0200

    ID-884 new backend data structure

--- test/mock/spid-talk_response-termsStatus-success.js
+++ test/mock/spid-talk_response-termsStatus-success.js
@@ -1,5 +1,5 @@
 function triggerResponse(){
-    var json = {"result":true,"serverTime":1390824533,"expiresIn":7111,"showPopup":true,"popupContent":"<h1>Hi</h1>"};
+    var json = {"result":true,"serverTime":1390824533,"expiresIn":7111,"showPopup":true,"popupData":{'header':'Hi!','logos':'','description':'SITENAME uses SPiD as a login service. SPiD has updated its Terms of Use and Privacy Policy.','notice':'If you do not accept the terms by YYYY-MM-DD, you will be logged out. Read more.','acceptText':'I accept the updated Terms of Use and Privacy Policy.','buttonText':'I accept the new Terms of Use.','declimeText':'Not now, remind me later'}};
 
     //Not part of response. Hack to get the callback id
     var scripts = document.head.getElementsByTagName('script');
@@ -18,6 +18,6 @@ function triggerResponse(){
     var id = src.match(/callback=[0-9a-f]+/i).pop().split("=").pop();
 
     if(global.SPiD) {
-        global.SPiD.Talk.response(id, {"result":true,"serverTime":1390824533,"expiresIn":7111,"showPopup":true,"popupContent":"<h1>Hi</h1>"});
+        global.SPiD.Talk.response(id, {"result":true,"serverTime":1390824533,"expiresIn":7111,"showPopup":true,"popupData":{'header':'Hi!','logos':'','description':'SITENAME uses SPiD as a login service. SPiD has updated its Terms of Use and Privacy Policy.','notice':'If you do not accept the terms by YYYY-MM-DD, you will be logged out. Read more.','acceptText':'I accept the updated Terms of Use and Privacy Policy.','buttonText':'I accept the new Terms of Use.','declimeText':'Not now, remind me later'}});
     }
 }(window));
